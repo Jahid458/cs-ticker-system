2ae09d5f306f27f9ff4c3c891ef4ee9579a99362
Jahid458 <Jahidul898@outlook.com>
2026-03-04 22:17:43 +0600

    header footer added

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Footer from './pages/Footer/Footer'
+import Navbar from './pages/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      
+      <Navbar />
+
+      <main className="grow">
+        
+      </main>
+
+      <Footer />
+
+    </div>
+  )
+}
+
+export default App
